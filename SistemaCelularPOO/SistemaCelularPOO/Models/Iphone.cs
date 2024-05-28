d9e30276eaf73588eee7a38ab266dbc8b7f0fdcc
Carolimaa <carolniver12@gmail.com>
2024-05-27 23:13:16 -0300

    Desafio Celular - DIO

---
+++ SistemaCelularPOO/SistemaCelularPOO/Models/Iphone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCelular.Models;
+
+public class Iphone : Smartphone
+{
+    public Iphone(string numero, string modelo, string iMEI, int memoria) : base(numero, modelo, iMEI, memoria)
+    {
+
+    }
+
+    public override void InstalarApp(string nomeApp)
+    {
+        Console.WriteLine($"Instalando o aplicativo {nomeApp} no Iphone");
+    }
+}
